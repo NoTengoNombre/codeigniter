59467edb3e81e6afd168b96af495cab7ccd9189c
Portatil_Bot <Portatil_Bot@192.168.1.36>
2016-10-15 11:46:57 +0200

    PHP 1

--- T2IntroduccionPHPYAccesoBD/EjerciciosBasicos/Ejercicio3/ejercicio3TablaMulti1ForModificado.php
+++ T2IntroduccionPHPYAccesoBD/EjerciciosBasicos/Ejercicio3/ejercicio3TablaMulti1ForModificado.php
@@ -8,17 +8,19 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="css/micssBasico.css">
-    <title>Tabla 1 </title>
+    <link rel="stylesheet" type="text/css" href="css/css.css">
+    <title>Tabla 1</title>
   </head>
   <body>
     <?php
     $contador = 0;
     echo "<table>";
+    echo "<thead> mensaje";
     for ($v1 = 0; $v1 <= 10; $v1++) {
       echo "<tr>";
-      echo "<th scope='col'>", $_REQUEST['numero'], ' x ', $contador, " = ", $_REQUEST['numero'] * $contador++, " </th>";
+      echo "<th>", $_REQUEST['numero'], ' x ', $contador, " = ", $_REQUEST['numero'] * $contador++, " </th>";
       echo "</tr>";
+      echo "</thead>";
     }
     echo "</table>";
     ?>
