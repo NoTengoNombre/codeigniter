81da0056655cd9e137b85278687f3ae3df3fcee9
Portatil_Bot <Portatil_Bot@192.168.1.36>
2016-10-18 19:30:32 +0200

    PHP Subida 2

---
+++ PHP_PruebasCodigo/PHP_Ejemplos_Codigo2015/PHP_Funcion_Var_dump/PHP_phpinfo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+    @Created on : 16-oct-2016, 23:00:57
+    @Author     : RVS - N.F.N.D
+    @Pag        :
+    @version    :
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    phpinfo();
+    
+    phpinfo(INFO_MODULES);
+    ?>
+  </body>
+</html>
