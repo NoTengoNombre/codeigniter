9b54b8bdfe0722a28ff48f6ba3d0be8104f14069
Portatil_Bot <Portatil_Bot@192.168.1.33>
2016-10-24 18:40:52 +0200

    PHP Subida Basica

---
+++ PHPBasica.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+    @Created on : 24-oct-2016, 18:39:25
+    @Author     : RVS - N.F.N.D
+    @Pag        :
+    @version    :
+    @TODO       :
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "valor";
+    ?>
+  </body>
+</html>
