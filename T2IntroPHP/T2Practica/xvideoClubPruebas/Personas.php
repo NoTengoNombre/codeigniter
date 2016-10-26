3c3dd9ab48ba47645ff7c16cde40d8acd155a1d5
Portatil_Bot <Portatil_Bot@192.168.1.33>
2016-10-26 11:26:29 +0200

    PHP practica videoclub Arreglar datos entrada Personas 4

--- T2IntroPHP/T2Practica/xvideoClubPruebas/Personas.php
+++ T2IntroPHP/T2Practica/xvideoClubPruebas/Personas.php
@@ -31,7 +31,9 @@
   <body>
     <form name="form1" method="get" action ="<?php echo $_SERVER['PHP_SELF'] ?>">
       <label><h3>Formulario</h3></label>
-      Cod_persona : <input type="text" name="cod_persona" value="">
+      Cod_persona : <input type="text" name="cod_persona2" value="">
+      <br>
+      Cod_persona a Cambiar : <input type="text" name="cod_persona" value="">
       <br>
       Nombre :  <input type="text" name="nombre" value="">
       <br>
@@ -53,6 +55,7 @@ class Personas {
 
 //   Atributos : Almacenan los valores del formulario cuando estos se envian
   public $cod_persona;
+  public $cod_persona2;
   public $nombre;
   public $apellidos;
   public $pais;
@@ -93,7 +96,7 @@ class Personas {
   /**
    * ♥ UPDATE
    */
-  public function modificar_personas() {
+  public function modificar_personas0() {
     $p = new Personas();
     $p->consultar_personas();
     echo "<br>";
@@ -103,10 +106,10 @@ class Personas {
       die("Error : No se establecido la conexion . " . $mysqli->connect_error);
     }
 //    Update
-    if ((isset($_REQUEST['cod_persona']) && isset($_REQUEST['nombre'])) && (isset($_REQUEST['apellidos']) && isset($_REQUEST['pais']))) {
-      if ((!empty($_REQUEST['cod_persona']) && !empty($_REQUEST['nombre'])) && (!empty($_REQUEST['apellidos']) && !empty($_REQUEST['pais']))) {
-
-        $consulta = "UPDATE Personas SET nombre = '" . $this->nombre . "' , apellidos = '" . $this->apellidos . "' , pais = '" . $this->pais . "' WHERE cod_persona = '" . $this->cod_persona . "';";
+    if ((isset($_REQUEST['cod_persona']) && isset($_REQUEST['nombre'])) && (isset($_REQUEST['apellidos']) && isset($_REQUEST['pais']) && isset($_REQUEST['cod_persona2']) )) {
+      if ((!empty($_REQUEST['cod_persona']) && !empty($_REQUEST['nombre'])) && (!empty($_REQUEST['apellidos']) && !empty($_REQUEST['pais']) && !empty($_REQUEST['cod_persona2']))) {
+        $consulta = "UPDATE personas SET cod_persona='" . $this->cod_persona . "', nombre='" . $this->nombre . "', apellidos='"
+                . $this->apellidos . "', pais='" . $this->pais . "' WHERE cod_persona = '" . $this->cod_persona2 . "';";
         if ($resultado = $mysqli->query($consulta)) {
           echo "<br><strong>Consulta realizada</strong><br>";
           while ($fila = $resultado->fetch_assoc()) {
@@ -125,24 +128,51 @@ class Personas {
         $mysqli->close();
       }
     }
+    echo "Error en el isset ";
   }
 
-}
+  /**
+   * ♥ UPDATE
+   */
+  public function modificar_personas() {
+    $p = new Personas();
+    $p->consultar_personas();
+    $db = new mysqli("localhost", "root", "", "videoclubprueba");
+    if ($db->connect_errno) {
+      die("Error : No se establecido la conexion . " . $db->connect_error);
+    }
+    if ((isset($_REQUEST['cod_persona']) && isset($_REQUEST['nombre'])) && (isset($_REQUEST['apellidos']) && isset($_REQUEST['pais']) && isset($_REQUEST['cod_persona2']) )) {
+      if ((!empty($_REQUEST['cod_persona']) && !empty($_REQUEST['nombre'])) && (!empty($_REQUEST['apellidos']) && !empty($_REQUEST['pais']) && !empty($_REQUEST['cod_persona2']))) {
+        $consulta = "UPDATE personas SET cod_persona='" . $this->cod_persona . "', nombre='" . $this->nombre . "', apellidos='"
+                . $this->apellidos . "', pais='" . $this->pais . "' WHERE cod_persona = '" . $this->cod_persona2 . "';";
+        $resultado = $db->query($consulta);
+        if ($resultado != 0) {
+          echo "<br><strong>Consulta realizada</strong><br>";
+          $resultado->free();
+          $db->close();
+        } else {
+          echo "<em>Atencion : No se pueden añadir campos <strong>vacios o nulos</strong></em><br>";
+          $db->close();
+        }
+      }
+      echo "Error en el isset ";
+    }
+
 
 // Objeto para añadir los datos del formulario a los atributos de clase 
-$per = new Personas();
+    $per = new Personas();
 
-if (isset($_REQUEST['enviar'])) {
+    if (isset($_REQUEST['enviar'])) {
 // Datos se envian formulario se almacenan atributos clase
-  $per->cod_persona = $_REQUEST['cod_persona'];
-  $per->nombre = $_REQUEST['nombre'];
-  $per->apellidos = $_REQUEST['apellidos'];
-  $per->pais = $_REQUEST['pais'];
-  $per->consulta_basica();
+      $per->cod_persona = $_REQUEST['cod_persona'];
+      $per->nombre = $_REQUEST['nombre'];
+      $per->apellidos = $_REQUEST['apellidos'];
+      $per->pais = $_REQUEST['pais'];
+      $per->consulta_basica();
 //  $per->consultar_personas();
 //  $per->aniadir_personas();
 //  $per->modificar_personas();
 //  $per->borrar_personas();
-}
-?>
+    }
 
+    
